b3e63c5eb12a037690a4cd53e430d88001f2dc47
parkhr <vlhong94@gmail.com>
2019-11-25 17:21:52 +0900

    Add vuex 기능개발, 메인페이지 추가

--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -4,10 +4,10 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
-
+import {store} from './store'
 Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
+  vuetify, store,
   render: h => h(App)
 }).$mount('#app')
